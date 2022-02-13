a17dc5ee699ef9518882d2305b7dadc11b524a87
marselinusharson <marselinus@undiksha.ac.id>
2022-02-13 17:00:47 +0800

    Merapikan kode menggguanakan fect()

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
-$(".search-button").on("click", function () {
-  $.ajax({
-    url: `http://www.omdbapi.com/?apikey=dbd23e51&s=${$(".input-keyword").val()}`,
-    success: (results) => {
-      const movies = results.Search;
+// $(".search-button").on("click", function () {
+//   $.ajax({
+//     url: `http://www.omdbapi.com/?apikey=dbd23e51&s=${$(".input-keyword").val()}`,
+//     success: (results) => {
+//       const movies = results.Search;
+
+//       let cards = "";
+//       movies.forEach((m) => {
+//         cards += showCards(m);
+//       });
+//       $(".movie-container").html(cards);
+
+//       //tombol details di klik
+//       $(".detail-button").on("click", function () {
+//         $.ajax({
+//           url: `http://www.omdbapi.com/?apikey=dbd23e51&i=${$(this).data("imdbid")}`,
+//           success: (m) => {
+//             const movieDetail = showMovieDetails(m);
+//             $(".modal-body").html(movieDetail);
+//           },
+//           error: (e) => {
+//             console.log(e.responseText);
+//           },
+//         });
+//       });
+//     },
+//     error: (e) => {
+//       console.log(e.responseText);
+//     },
+//   });
+// });
+
+//Gunakan fetch
+const searchButton = document.querySelector(".search-button");
+const keyword = document.querySelector(".input-keyword");
+searchButton.addEventListener("click", function () {
+  fetch("http://www.omdbapi.com/?apikey=dbd23e51&s=" + keyword.value)
+    .then((response) => response.json())
+    .then((response) => {
+      const movies = response.Search;
 
       let cards = "";
       movies.forEach((m) => {
         cards += showCards(m);
       });
-      $(".movie-container").html(cards);
-
-      //tombol details di klik
-      $(".detail-button").on("click", function () {
-        $.ajax({
-          url: `http://www.omdbapi.com/?apikey=dbd23e51&i=${$(this).data("imdbid")}`,
-          success: (m) => {
-            const movieDetail = showMovieDetails(m);
-            $(".modal-body").html(movieDetail);
-          },
-          error: (e) => {
-            console.log(e.responseText);
-          },
+      const movieContainer = document.querySelector(".movie-container");
+      movieContainer.innerHTML = cards;
+      //ketika tombol detail diklik
+      const modalDetail = document.querySelectorAll(".detail-button");
+      modalDetail.forEach((btn) => {
+        btn.addEventListener("click", function () {
+          const imdbid = this.dataset.imdbid;
+          fetch("http://www.omdbapi.com/?apikey=dbd23e51&i=" + imdbid)
+            .then((response) => response.json())
+            .then((m) => {
+              const movielDetail = showMovieDetails(m);
+              const modalBody = document.querySelector(".modal-body");
+              modalBody.innerHTML = movielDetail;
+            });
         });
       });
-    },
-    error: (e) => {
-      console.log(e.responseText);
-    },
-  });
+    });
 });
 
 function showMovieDetails(m) {
@@ -39,7 +71,7 @@ function showMovieDetails(m) {
             </div>
             <div class="col-md">
                 <ul class="list-group">
-                        <li class="list-group-item"><h1>${m.Title}</h1></li>
+                        <li class="list-group-item"><h2>${m.Title}</h2></li>
                         <li class="list-group-item"><strong>Directors :</strong>${m.Director}</li>
                         <li class="list-group-item"><strong>Actors: </strong>${m.Actors}</li>
                         <li class="list-group-item"><strong>Plot:</strong><br> ${m.Plot}</li>
